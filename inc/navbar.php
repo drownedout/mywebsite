1fb131a54da462ccfce56bae5d2e2f5e399615b2
drownedout <danielcdonnelly@gmail.com>
2015-06-11 14:23:49 -0700

    inital commit

---
+++ inc/navbar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Daniel Donnelly</title>
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="./css/flat-ui.css">
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse" role="navigation">
+	  <div class="navbar-header">
+	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+	      <span class="sr-only">Toggle navigation</span>
+	    </button>
+	    <a class="navbar-brand" href="#">Flat UI</a>
+	  </div>
+	  <div class="collapse navbar-collapse" id="navbar-collapse-01">
+	    <ul class="nav navbar-nav">
+	      <li><a href="#fakelink">Features</a></li>
+	      <li><a href="#fakelink">Features</a></li>
+	      <li><a href="./photography.html">Photography</a></li>
+	    </ul>
+	    <form class="navbar-form navbar-right" action="#" role="search">
+	      <div class="form-group">
+	        <div class="input-group">
+	          <input class="form-control" id="navbarInput-01" type="search" placeholder="Search">
+	          <span class="input-group-btn">
+	            <button type="submit" class="btn"><span class="fui-search"></span></button>
+	          </span>
+	        </div>
+	      </div>
+	    </form>
+	  </div><!-- /.navbar-collapse -->
+	</nav><!-- /navbar -->
