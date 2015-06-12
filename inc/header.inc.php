03bfe1c2544975049162e563b69381ed6e06bf8b
drownedout <danielcdonnelly@gmail.com>
2015-06-12 13:35:53 -0700

    Added animations, changed style, etc

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -10,24 +10,32 @@
 <body>
 	<nav class="navbar navbar-inverse" role="navigation">
 	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
-	      <span class="sr-only">Toggle navigation</span>
-	    </button>
 	    <a class="navbar-brand" href="#">Home</a>
 	  </div>
 	  <div class="collapse navbar-collapse" id="navbar-collapse-01">
 	    <ul class="nav navbar-nav">
-	      <li><a href="#">Rails</a></li>
+	    <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tutorials <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">Ruby</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Javascript</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Design</a></li>
+          </ul>
+        </li>
 	      <li><a href="#">Data</a></li>
 	      <li><a href="#">Photography</a></li>
 	    </ul>
 	    <div class="navbar-form navbar-right">
 	      <div class="form-group">
 	        <div class="input-group">
-	         <span class="icon-github icon-large"></span>
+	         <a href="https://github.com/drownedout"><span class="icon-github icon-large"></span></a>
 	         <span class="icon-facebook icon-large"></span>
-	         <span class="icon-linked-in icon-large"></span> 
-	         <span class="icon-flickr icon-large"></span> 
+	         <a href="https://www.linkedin.com/pub/daniel-donnelly/32/717/520"><span class="icon-linked-in icon-large"></span></a>
+	         <a href="https://www.flickr.com/dcdonnelly"><span class="icon-flickr icon-large"></span></a> 
 	        </div>
 	      </div>
 	    </form>
--- index.php
+++ index.php
@@ -1,48 +1,17 @@
 <?php include ("./inc/header.inc.php"); ?>
 	<div class="container">
 		<div class="jumbotron">
-			<h1>You Name Here</h1>
+			<h2 class="box fade-in one">Hello!</h2><h2 class="box fade-in two">My name is Daniel</h2>
 		</div>
-	</div>
-	<div class="container">
 		<div class="row">
-			<h3>About</h3>
+			<h3>About Me</h3>
 				<p>Proin condimentum ex eu commodo iaculis. Ut commodo ornare volutpat. Praesent urna dolor, venenatis sit amet mattis nec, tristique et velit. Sed pharetra condimentum metus, id sagittis erat molestie sed. Etiam et malesuada urna. Sed et aliquet turpis. Aenean tristique efficitur orci, molestie dignissim ligula hendrerit sollicitudin. Morbi bibendum rhoncus orci. Suspendisse laoreet diam facilisis facilisis convallis. Aliquam efficitur auctor placerat. Nunc cursus viverra libero ac pulvinar. Nunc ut dolor quis ipsum eleifend posuere.In vulputate magna non elit tincidunt fringilla. Praesent placerat, enim sed eleifend dictum, velit sem pellentesque enim, a auctor dui mauris non dui. Mauris ultrices neque nec lorem fermentum eleifend.</p>
-
-				<div class="btn-group">
-  <button class="btn btn-inverse dropdown-toggle" type="button" data-toggle="dropdown">
-    Large button <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu dropdown-menu-inverse" role="menu">
-    <li><a href="#">Action</a></li>
-    <li><a href="#">Another action</a></li>
-    <li><a href="#">Something else here</a></li>
-    <li class="divider"></li>
-    <li><a href="#">Separated link</a></li>
-  </ul>
-</div>
 		</div>
-		<div class="container">
-			<div class="col-md-6" id="right_col">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-			<div class="col-md-4" id="left_col">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
 	</div>
 <footer>
 <script src="./js/jquery.js"></script>
 <script type="text/javascript" src="./js/bootstrap.js"></script>
+<script type="text/javascript" src="./js/index.js"></script>
 </footer>
 </body>
 </html>
