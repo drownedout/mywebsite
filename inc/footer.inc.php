e95906333d0404a3368f96187a9205ab1555d0e2
drownedout <danielcdonnelly@gmail.com>
2015-06-16 14:56:56 -0700

    Footer added/separated

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,6 +2,7 @@
 <script src="./js/jquery.js"></script>
 <script type="text/javascript" src="./js/bootstrap.js"></script>
 <script type="text/javascript" src="./js/header.js"></script>
+<span>2015 Blog Title</span>
 </footer>
 </body>
 </html>
